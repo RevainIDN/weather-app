e2100730657159e8c23efb08669f6507a00507b1
RevainIDN <kov.igor94@gmail.com>
2025-01-17 17:08:03 +0200

    Добавлена настройка параметров данных получаемых пользователем

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import '../styles/Header.css'
 import search from '/Search.svg'
 
-export default function Header() {
+interface HeaderProps {
+	handleInputText: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+	handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Header({ handleInputText, handleChange }: HeaderProps) {
 	return (
 		<div className='header'>
 			<div className='input-cont'>
 				<img className='input-img' src={search} alt="Search" />
-				<input className='input' type="text" maxLength={30} placeholder='Search city...' />
+				<input className='input' type="text" maxLength={30} placeholder='Search city...' onKeyDown={handleInputText} />
 			</div>
 			<div className='switch-cont'>
 				<label className='switch'>
-					<input className='switch-input' type="checkbox" />
+					<input className='switch-input' type="checkbox" onChange={handleChange} />
 					<span className='switch-slider'>
 						<span className='switch-text switch-text-left'>°C</span>
 						<span className='switch-text switch-text-right'>°F</span>
