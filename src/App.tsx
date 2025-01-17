ec2bb94a49ed0b99921a55a3ff605224380a2c90
RevainIDN <kov.igor94@gmail.com>
2025-01-17 17:05:45 +0200

    Реализована отправка GET запроса на сервер и получение данных

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { CurrentCityDatas, ForecastCityDatas, transformCurrentWeatherData, transformForecastWeatherData } from './types'
 import Header from './components/Header'
 import CurrentWeather from './components/CurrentWeather'
 import HourlyForecast from './components/HourlyForecast'
@@ -7,11 +8,57 @@ import CityWeather from './components/CityWeather'
 import DailyForecast from './components/DailyForecast'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [currentWeatherData, setCurrentWeatherData] = useState<CurrentCityDatas | null>(null);
+  const [forecastWeatherData, setForecastWeatherData] = useState<ForecastCityDatas | null>(null);
+  const [currentCity, setCurrentCity] = useState<string>('London');
+  const [units, setUnits] = useState<'metric' | 'imperial'>('metric');
+
+  const handleInputText = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setCurrentCity(e.currentTarget.value);
+    }
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUnits(e.target.checked ? 'imperial' : 'metric');
+  }
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const apiKey = `64c63b06c0e83ae035a8daeecb963955`;
+
+      try {
+        const responseCurrentWeather = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?units=${units}&lang=en&q=${currentCity}&appid=${apiKey}`
+        );
+        const currentData = await responseCurrentWeather.json();
+
+        const transformedCurrentData = transformCurrentWeatherData(currentData);
+        setCurrentWeatherData(transformedCurrentData);
+
+        const responseForecast = await fetch(
+          `https://api.openweathermap.org/data/2.5/forecast?units=${units}&q=${currentCity}&appid=${apiKey}`
+        );
+        const forecastData = await responseForecast.json();
+
+        const transformedForecastData = transformForecastWeatherData(forecastData);
+        setForecastWeatherData(transformedForecastData);
+      } catch (error) {
+        console.error('An error occurred while retrieving data: ', error);
+      }
+    }
+
+    fetchWeatherData();
+  }, [currentCity, units])
+  console.log(currentWeatherData);
+  console.log(forecastWeatherData);
 
   return (
     <div className='weather-app'>
-      <Header />
+      <Header
+        handleInputText={handleInputText}
+        handleChange={handleChange}
+      />
       <div className='first-weather-forecast'>
         <CurrentWeather />
         <HourlyForecast />
